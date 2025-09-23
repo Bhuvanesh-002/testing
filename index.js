e398c974b84e2d4289e3b7543cd20a9edff6c084
BHUVANESH B <146980317+Bhuvanesh-002@users.noreply.github.com>
2025-09-23 11:55:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+<html>
+  <head>
+	<title> Cross frame scripting</title>
+  </head>
+  <body>
+	<p>Check for website vulnerable for cross frame scripting</p>
+	<script>alert("Cross Domain Script included")</script>
+</body>
+</html>
